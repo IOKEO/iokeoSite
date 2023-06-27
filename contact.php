f733444982a4661c439f88ecee317f6e84a9f290
AmosAdw <ada@iokeo.com>
2023-06-27 12:04:37 +0100

    commit 6

--- contact.php
+++ contact.php
@@ -32,7 +32,7 @@ if(isset($_POST['submit'])){
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <link href="img/k.png" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
